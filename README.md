cec89a98b13c21a4d0b9ee874a99e0f82316268a
123woscc <1284511710@qq.com>
2018-05-25 18:42:58 +0800

    v0.1

---
+++ README.md
@@ -0,0 +1,47 @@
+# 手机查看电脑的图片
+
+## 起因
+
+为了看漫画下载了漫画合集，但是合集一般都很大而且是压缩文件，手机比较渣解压的比较满，而且还占空间；
+
+因此可以通过电脑下载漫画合集解压后通过该脚本搭建一个简易的图片浏览网站来看漫画~~~
+
+
+
+## 依赖
+
+`pip install flask`
+
+
+
+## 使用
+
+`python app.py <path>`
+
+eg:
+
+`python app.py C:\迅雷下载`
+
+
+
+## 访问
+
+1、手机连接局域网；
+
+2、cmd下执行ipconfig查看电脑IP地址
+
+![](https://ws1.sinaimg.cn/large/006oqg34gy1frnrul778ej30y6077mxt.jpg)
+
+3、手机打开浏览器输入： `ip:5000` 即可查看效果
+
+
+
+## 效果
+
+目录：
+
+![](https://ws1.sinaimg.cn/large/006oqg34gy1frnrn0xmb3j30q00j4mza.jpg)
+
+图片：
+
+![](https://ws1.sinaimg.cn/large/006oqg34gy1frnrp5d4uej30c40nr497.jpg)
---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+from flask import Flask, render_template, send_file
+
+
+def get_all_path(dirname):
+    result = []
+    for root, dirs, _ in os.walk(dirname):
+        for dir in dirs:
+            path = os.path.join(root, dir)
+            result.append(path)
+    return result
+
+
+app = Flask(__name__)
+# 获取根目录
+app.config['DIRPATH'] = sys.argv[1]
+
+
+# 获取文件夹目录
+@app.route('/')
+def index():
+    dirpath = app.config['DIRPATH']
+    paths = get_all_path(dirpath)
+    return render_template('index.html', paths=paths)
+
+
+# 显示图片
+@app.route('/view/<path>')
+def view(path):
+    img_check = ['.jpg', '.png']
+    imgs = []
+    files = os.listdir(path)
+    for file in files:
+        if os.path.splitext(file)[1] in img_check:
+            imgs.append(file)
+    imgs = sorted(imgs)
+    imgs = [os.path.join(path, img) for img in imgs]
+    return render_template('view.html', imgs=imgs)
+
+
+# 返回图片的真实地址
+@app.route('/show/<path:path>')
+def show(path):
+    return send_file(path, mimetype='image/jpeg')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
